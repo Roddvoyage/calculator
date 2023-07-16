c3da6e0084c68f7ff35e87ab0aa39078d6aa2e12
Roddvoyage <joao.f.rodrigues.93@gmail.com>
2023-07-16 23:53:48 +0100

    first files

---
+++ javascript.js
@@ -0,0 +1,42 @@
+class Calculator {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+let currentScreen = document.getElementById('currentScreen');
+let previousScreen = document.getElementById('previousScreen');
+let clear = document.getElementById('clear');
+let delete1 = document.getElementById('delete');
+let multiply = document.getElementById('multiply');
+let divide = document.getElementById('divide');
+let add = document.getElementById('add');
+let subtract = document.getElementById('substract');
+
+let equal = document.getElementById('equal');
+let previousScreen = document.getElementById('previousScreen');
+let dot = document.getElementById('dot');
+let nr1 = document.getElementById('nr1');
+let multiply = document.getElementById('multiply');
+let divide = document.getElementById('divide');
+let add = document.getElementById('add');
+let subtract = document.getElementById('substract');
